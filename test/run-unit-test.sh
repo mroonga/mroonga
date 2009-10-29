e2e58077bddeff5512712d4c18ce12b661c27b80
Tetsuro IKEDA <te.ikeda@jpta.scs.co.jp>
2009-10-29 20:11:36 +0900

    upgrade test suite script for easy gdb debugging. (thanks to Sutou-san.)

--- test/run-unit-test.sh
+++ test/run-unit-test.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
 
 export BASE_DIR="`dirname $0`"
+top_dir="$BASE_DIR/.."
 
 if test -z "$NO_MAKE"; then
-    make -C $BASE_DIR/../ > /dev/null || exit 1
+    make -C $top_dir > /dev/null || exit 1
 fi
 
 if test -z "$CUTTER"; then
     CUTTER="`make -s -C $BASE_DIR echo-cutter`"
 fi
+export CUTTER
 
-$CUTTER --keep-opening-modules -s $BASE_DIR "$@" $BASE_DIR
+CUTTER_ARGS=
+CUTTER_WRAPPER=
+if test x"$CUTTER_DEBUG" = x"yes"; then
+    CUTTER_WRAPPER="$top_dir/libtool --mode=execute gdb --args"
+    CUTTER_ARGS="--keep-opening-modules"
+elif test x"$CUTTER_CHECK_LEAK" = x"yes"; then
+    CUTTER_WRAPPER="$top_dir/libtool --mode=execute valgrind "
+    CUTTER_WRAPPER="$CUTTER_WRAPPER --leak-check=full --show-reachable=yes -v"
+    CUTTER_ARGS="--keep-opening-modules"
+fi
+
+CUTTER_ARGS="$CUTTER_ARGS -s $BASE_DIR"
+$CUTTER_WRAPPER $CUTTER $CUTTER_ARGS "$@" $BASE_DIR
