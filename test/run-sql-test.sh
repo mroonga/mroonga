83f6112bf42b5ddcc33a2f7ccecf328e82c42ea3
Tetsuro IKEDA <ikdttr@gmail.com>
2010-03-17 15:04:19 +0900

    added test SQL script for "make check".

---
+++ test/run-sql-test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if test -n "$MYSQL_HOME"; then
+    SQL_TESTS="$MYSQL_HOME/mysql-test/suite/mroonga"
+    if test -d "$SQL_TESTS"; then
+	cd $MYSQL_HOME/mysql-test
+	./mysql-test-run.pl --suite=mroonga --force
+    else
+	echo "$SQL_TESTS does not exists. Aborting SQL test."
+    fi
+else
+    echo '$MYSQL_HOME is not defined. Aborting SQL test.'
+fi
