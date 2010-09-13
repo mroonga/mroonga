fedf5865c9e49a12f11ac2363171b8062ce5a885
Kouhei Sutou <kou@clear-code.com>
2010-09-13 11:20:28 +0900

    install plugin for testing.

--- test/run-sql-test.sh
+++ test/run-sql-test.sh
@@ -13,10 +13,14 @@ local_groonga_mysql_test_suite_dir="${BASE_DIR}/sql"
 mysql_test_dir="${MYSQL_BUILD}/mysql-test"
 test_suites_dir="${mysql_test_dir}/suite"
 groonga_mysql_test_suite_dir="${test_suites_dir}/${test_suite_name}"
+plugins_dir="${MYSQL_BUILD}/lib/mysql/plugin"
 
 if ! test -e "${groonga_mysql_test_suite_dir}"; then
     ln -s "${local_groonga_mysql_test_suite_dir}" \
 	"${groonga_mysql_test_suite_dir}"
 fi
+
+make -C ${top_dir} install-pluginLTLIBRARIES plugindir=${plugins_dir}
+
 (cd "$mysql_test_dir" && \
     ./mysql-test-run.pl --suite="${test_suite_name}" --force)
