f3e1059010596b7e5eb131d31c1bc4bfbb25f2f3
Kouhei Sutou <kou@clear-code.com>
2011-09-25 12:20:14 +0900

    fix crash bug on delete table. fixes #1063
    It will be caused on patition storage engine enabled MySQL
    5.1.X like Ubuntu 10.04.
    
    Reported by Isao Sugimoto. Thanks!!!

--- ha_mroonga.cc
+++ ha_mroonga.cc
@@ -2501,7 +2501,7 @@ int ha_mroonga::delete_table(const char *name)
   /* This is previous version */
   tmp_table_share->version--;
   tmp_table.s = tmp_table_share;
-#if MYSQL_VERSION_ID >= 50500
+#ifdef WITH_PARTITION_STORAGE_ENGINE
   tmp_table.part_info = NULL;
 #endif
   if (!(tmp_share = mrn_get_share(name, &tmp_table, &error)))
