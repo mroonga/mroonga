9ac4941234587bd295f3a716f0a3a506dfaba9df
Kouhei Sutou <kou@clear-code.com>
2011-11-22 16:42:07 +0900

    [geo] use unlink again because link accepts nil ID.
    See also commit:c0cb00c64c814121f87161f8edb18fddf05653d6 in groonga.

--- ha_mroonga.cc
+++ ha_mroonga.cc
@@ -5624,7 +5624,7 @@ void ha_mroonga::clear_cursor_geo()
 {
   MRN_DBUG_ENTER_METHOD();
   if (geo_cursor) {
-    grn_obj_close(ctx, geo_cursor);
+    grn_obj_unlink(ctx, geo_cursor);
     geo_cursor = NULL;
   }
   DBUG_VOID_RETURN;
