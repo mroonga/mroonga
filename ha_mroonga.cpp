26e78ff2d48f32049206b5920efcc96d9ab7f2f1
Kouhei Sutou <kou@clear-code.com>
2012-04-19 00:08:06 +0900

    storage: use unsigined type for SET type
    Unsigned type is suitable because SET value is bit string.

--- ha_mroonga.cpp
+++ ha_mroonga.cpp
@@ -1033,7 +1033,7 @@ static grn_builtin_type mrn_grn_type_from_field(grn_ctx *ctx, Field *field,
     type = GRN_DB_UINT16;       // 2bytes
     break;
   case MYSQL_TYPE_SET:          // SET; <= 8bytes
-    type = GRN_DB_INT64;        // 8bytes
+    type = GRN_DB_UINT64;       // 8bytes
     break;
   case MYSQL_TYPE_TINY_BLOB:    // TINYBLOB; <= 256bytes + 1byte
     type = GRN_DB_SHORT_TEXT;   // 4Kbytes
