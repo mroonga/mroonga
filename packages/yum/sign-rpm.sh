4cf0435fc649dd5185288d1569bb7d6d0e4a8749
Kouhei Sutou <kou@clear-code.com>
2012-04-29 09:18:39 +0900

    set GPG sign key in configure

--- packages/yum/sign-rpm.sh
+++ packages/yum/sign-rpm.sh
@@ -2,13 +2,14 @@
 
 script_base_dir=`dirname $0`
 
-if [ $# != 1 ]; then
-    echo "Usage: $0 DISTRIBUTIONS"
-    echo " e.g.: $0 'fedora centos'"
+if [ $# != 2 ]; then
+    echo "Usage: $0 GPG_UID DISTRIBUTIONS"
+    echo " e.g.: $0 'F10399C0' 'fedora centos'"
     exit 1
 fi
 
-DISTRIBUTIONS=$1
+GPG_UID=$1
+DISTRIBUTIONS=$2
 
 run()
 {
@@ -19,8 +20,16 @@ run()
     fi
 }
 
+rpms=""
 for distribution in ${DISTRIBUTIONS}; do
-    run rpm -D "_gpg_name `$script_base_dir/gpg-uid.sh`" \
-	--resign $script_base_dir/${distribution}/*/*/*/*.rpm &
+    rpms="${rpms} $(echo $script_base_dir/${distribution}/*/*/*/*.rpm)"
 done
-wait
+
+echo "NOTE: YOU JUST ENTER! YOU DON'T NEED TO INPUT PASSWORD!"
+echo "      IT'S JUST FOR rpm COMMAND RESTRICTION!"
+run rpm \
+    -D "_gpg_name ${GPG_UID}" \
+    -D "__gpg /usr/bin/gpg2" \
+    -D "__gpg_check_password_cmd /bin/true true" \
+    -D "__gpg_sign_cmd %{__gpg} gpg --batch --no-verbose --no-armor %{?_gpg_digest_algo:--digest-algo %{_gpg_digest_algo}} --no-secmem-warning -u \"%{_gpg_name}\" -sbo %{__signature_filename} %{__plaintext_filename}" \
+    --resign $rpms
