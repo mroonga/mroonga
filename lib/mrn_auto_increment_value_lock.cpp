46f82eb903a496f942466209592d9a278237694b
Kouhei Sutou <kou@clear-code.com>
2012-05-07 19:17:40 +0900

    mysql5.1: make buildable but alter_table_disable_enable_keys still fails
    refs #1310 #1311

--- lib/mrn_auto_increment_value_lock.cpp
+++ lib/mrn_auto_increment_value_lock.cpp
@@ -19,18 +19,24 @@
 
 #include "mrn_auto_increment_value_lock.hpp"
 
+#if MYSQL_VERSION_ID >= 50500
+#  define AUTO_INCREMENT_VALUE_MUTEX(table_share) (&(table_share->LOCK_ha_data))
+#else
+#  define AUTO_INCREMENT_VALUE_MUTEX(table_share) (&(table_share->mutex))
+#endif
+
 namespace mrn {
   AutoIncrementValueLock::AutoIncrementValueLock(TABLE_SHARE *table_share)
     : table_share_(table_share),
       need_lock_(table_share_->tmp_table == NO_TMP_TABLE) {
     if (need_lock_) {
-      mysql_mutex_lock(&table_share_->LOCK_ha_data);
+      mysql_mutex_lock(AUTO_INCREMENT_VALUE_MUTEX(table_share_));
     }
   }
 
   AutoIncrementValueLock::~AutoIncrementValueLock() {
     if (need_lock_) {
-      mysql_mutex_unlock(&table_share_->LOCK_ha_data);
+      mysql_mutex_unlock(AUTO_INCREMENT_VALUE_MUTEX(table_share_));
     }
   }
 }
