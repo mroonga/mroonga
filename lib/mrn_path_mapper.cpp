882a122fcd8e1dc7c50882c23e96c34240ed0957
HAYASHI Kentaro <hayashi@clear-code.com>
2012-06-28 10:11:15 +0900

    Changes variable name to more meaningful one

--- lib/mrn_path_mapper.cpp
+++ lib/mrn_path_mapper.cpp
@@ -50,8 +50,8 @@ namespace mrn {
 
     if (strncmp(mysql_path_, "./", 2) == 0) {
       if (path_prefix_) {
-        size_t db_len = strlen(db_path_);
-        strncat(db_path_, path_prefix_, MRN_MAX_PATH_SIZE - db_len - 1);
+        size_t db_path_length = strlen(db_path_);
+        strncat(db_path_, path_prefix_, MRN_MAX_PATH_SIZE - db_path_length - 1);
       }
 
       int i = 2, j = strlen(db_path_), len;
