b894e8c769386e1990dc650f62b494406fa7d62e
Kouhei Sutou <kou@clear-code.com>
2012-09-10 15:37:37 +0900

    travis: install MariaDB by package
    Because it takes long build time.

--- tools/travis_before_script.sh
+++ tools/travis_before_script.sh
@@ -3,38 +3,9 @@
 set -x
 set -e
 
-mkdir -p vendor
-cd vendor
-if [ "$MYSQL_VERSION" = "system" ]; then
-    sudo apt-get -y build-dep mysql-server
-    apt-cache show mysql-server
-    sudo cat /etc/apt/sources.list
-    sudo cat /etc/apt/sources.list.d/*.list
-    apt-get source mysql-server
-    ln -s $(find . -maxdepth 1 -type d | sort | tail -1) mysql
-    cd mysql
-    debuild -us -uc -Tconfigure
-    make -j$(grep '^processor' /proc/cpuinfo | wc -l) > /dev/null
-    cd ..
-else
-    sudo apt-get -y install cmake
-    if [ -n "$MARIADB_VERSION" ]; then
-	wget http://mirror3.layerjet.com/mariadb/mariadb-${MARIADB_VERSION}/kvm-tarbake-jaunty-x86/mariadb-${MARIADB_VERSION}.tar.gz
-	tar xzf mariadb-${MARIADB_VERSION}.tar.gz
-	ln -s mariadb-${MARIADB_VERSION} mysql
-    else
-	wget http://cdn.mysql.com/Downloads/MySQL-5.5/mysql-${MYSQL_VERSION}.tar.gz
-	tar xzf mysql-${MYSQL_VERSION}.tar.gz
-	ln -s mysql-${MYSQL_VERSION} mysql
-    fi
-    cd mysql
-    BUILD/compile-amd64-max
-    cd ..
-fi
-cd ..
-
 touch NEWS # For old automake
 ./autogen.sh
+
+PATH=$PWD/vendor/mysql/scripts:$PATH
 ./configure \
-    --with-mysql-source=$PWD/vendor/mysql \
-    --with-mysql-config=$PWD/vendor/mysql/scripts/mysql_config
+    --with-mysql-source=$PWD/vendor/mysql
---
+++ tools/travis_install_mysql.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -x
+set -e
+
+mkdir -p vendor
+cd vendor
+if [ "$MYSQL_VERSION" = "system" ]; then
+    sudo apt-get -y build-dep mysql-server
+    sudo apt-get -y -V install mysql-server
+    apt-get source mysql-server
+    ln -s $(find . -maxdepth 1 -type d | sort | tail -1) mysql
+elif [ -n "$MARIADB_VERSION" ]; then
+    distribution=$(lsb_release --short --id | tr 'A-Z' 'a-z')
+    code_name=$(lsb_release --short --codename)
+    component=main
+    seriese=$(echo "$MARIADB_VERSION" | sed -e 's/\.[0-9]*$//g')
+    apt_url_base="http://ftp.osuosl.org/pub/mariadb/repo/${seriese}"
+    cat <<EOF | sudo tee /etc/apt/sources.list.d/mariadb.list
+deb ${apt_url_base}/${distribution}/ ${code_name} ${component}
+deb-src ${apt_url_base}/${distribution}/ ${code_name} ${component}
+EOF
+    sudo apt-get -y build-dep mysql-server
+    sudo apt-get -y -V install mysql-server
+    apt-get source mysql-server
+    ln -s $(find . -maxdepth 1 -type d | sort | tail -1) mysql
+else
+    sudo apt-get -y install cmake
+    wget http://cdn.mysql.com/Downloads/MySQL-5.5/mysql-${MYSQL_VERSION}.tar.gz
+    tar xzf mysql-${MYSQL_VERSION}.tar.gz
+    ln -s mysql-${MYSQL_VERSION} mysql
+    cd mysql
+    BUILD/compile-amd64-max
+    cd ..
+fi
+cd ..
