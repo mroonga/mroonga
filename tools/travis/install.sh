f616fd5541ce796aaa7c1670767641f9fb5c265c
Kouhei Sutou <kou@clear-code.com>
2012-10-23 12:34:33 +0900

    travis: use packages

--- tools/travis/install.sh
+++ tools/travis/install.sh
@@ -5,36 +5,46 @@ set -e
 
 curl https://raw.github.com/groonga/groonga/master/data/travis/setup.sh | sh
 curl https://raw.github.com/clear-code/cutter/master/data/travis/setup.sh | sh
-sudo apt-get -y install groonga-tokenizer-mecab
+sudo apt-get -qq -y install groonga-tokenizer-mecab
 
 mkdir -p vendor
 cd vendor
-if [ "$MYSQL_VERSION" = "system" ]; then
-    sudo apt-get -y build-dep mysql-server
-    sudo apt-get -y -V install mysql-server
-    apt-get source mysql-server
-    ln -s $(find . -maxdepth 1 -type d | sort | tail -1) mysql
-elif [ -n "$MARIADB_VERSION" ]; then
-    distribution=$(lsb_release --short --id | tr 'A-Z' 'a-z')
-    code_name=$(lsb_release --short --codename)
-    component=main
-    seriese=$(echo "$MARIADB_VERSION" | sed -e 's/\.[0-9]*$//g')
-    apt_url_base="http://ftp.osuosl.org/pub/mariadb/repo/${seriese}"
-    cat <<EOF | sudo tee /etc/apt/sources.list.d/mariadb.list
+
+version=$(echo "$MYSQL_VERSION" | sed -e 's/^\(mysql\|mariadb\)-//')
+series=$(echo "$version" | sed -e 's/\.[0-9]*\(-[a-z]*\)\?$//g')
+case "$MYSQL_VERSION"; in
+    mysql-*)
+	sudo apt-get -qq -y build-dep mysql-server
+	if [ "$version" = "system" ]; then
+	    sudo apt-get -qq -y install mysql-server mysql-testsuite
+	    apt-get -qq source mysql-server
+	    ln -s $(find . -maxdepth 1 -type d | sort | tail -1) mysql
+	else
+	    download_base="http://cdn.mysql.com/Downloads/MySQL-${series}/"
+	    deb=mysql-${version}-debian6.0-i686.deb
+	    tar_gz=mysql-${version}.tar.gz
+	    curl -O ${download_base}${deb} &
+	    curl -O ${download_base}${tar_gz} &
+	    wait
+	    sudo dpkg -i $deb
+	    tar xzf $tar_gz
+	    ln -s mysql-${version} mysql
+	fi
+	;;
+    mariadb-*)
+	distribution=$(lsb_release --short --id | tr 'A-Z' 'a-z')
+	code_name=$(lsb_release --short --codename)
+	component=main
+	apt_url_base="http://ftp.osuosl.org/pub/mariadb/repo/${series}"
+	cat <<EOF | sudo tee /etc/apt/sources.list.d/mariadb.list
 deb ${apt_url_base}/${distribution}/ ${code_name} ${component}
 deb-src ${apt_url_base}/${distribution}/ ${code_name} ${component}
 EOF
-    sudo apt-get -y build-dep mysql-server
-    sudo apt-get -y -V install mysql-server
-    apt-get source mysql-server
-    ln -s $(find . -maxdepth 1 -type d | sort | tail -1) mysql
-else
-    sudo apt-get -y install cmake
-    wget http://cdn.mysql.com/Downloads/MySQL-5.5/mysql-${MYSQL_VERSION}.tar.gz
-    tar xzf mysql-${MYSQL_VERSION}.tar.gz
-    ln -s mysql-${MYSQL_VERSION} mysql
-    cd mysql
-    BUILD/compile-amd64-max
-    cd ..
-fi
+	sudo apt-get -qq -y build-dep mysql-server
+	sudo apt-get -qq -y install mysql-server mysql-testsuite
+	apt-get -qq source mysql-server
+	ln -s $(find . -maxdepth 1 -type d | sort | tail -1) mysql
+	;;
+esac
+
 cd ..
---
+++ tools/travis/script.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+set -x
+set -e
+
+base_dir="$(cd $(dirname $0); pwd)"
+top_dir="$base_dir/../.."
+n_processors="$(grep '^processor' /proc/cpuinfo | wc -l)"
+. "${top_dir}/config.sh"
+
+build()
+{
+    make -j${n_processors} > /dev/null
+}
+
+run_unit_test()
+{
+    NO_MAKE=yes test/run-unit-test.sh
+}
+
+prepare_mysql_test_dir()
+{
+    if [ -d /usr/lib/mysql-testsuite/ ]; then
+	sudo cp -a /usr/lib/mysql-testsuite/ /usr/mysql-test/
+    else
+	sudo cp -a ${MYSQL_SOURCE_DIR}/mysql-test/ /usr/mysql-test/
+    fi
+    sudo chown -R $(id -u):$(id -g) /usr/mysql-test
+
+    cp -a ${top_dir}/test/sql/include/*.inc /usr/mysql-test/include/
+    cp -a ${top_dir}/test/sql/suite/mroonga/ /usr/mysql-test/suite/
+}
+
+collect_test_suite_names()
+{
+    cd /usr/mysql-test/suite/mroonga/
+    test_suite_names=""
+    for test_suite_name in $(find mroonga -type d '!' -name '[tr]'); do
+	if [ -n "${test_suite_names}" ]; then
+	    test_suite_names="${test_suite_names},"
+	fi
+	test_suite_names="${test_suite_names}${test_suite_name}"
+    done
+    cd -
+}
+
+prepare_sql_test()
+{
+    sudo make install > /dev/null
+    prepare_mysql_test_dir
+    collect_test_suite_names
+}
+
+run_sql_test()
+{
+    prepare_sql_test
+
+    cd /usr/mysql-test
+    ./mysql-test-run.pl \
+	--no-check-testcases \
+	--parallel="${n_processors}" \
+	--retry=1 \
+	--suite="${test_suite_names}" \
+	--force
+}
+
+build
+run_unit_test
+run_sql_test
