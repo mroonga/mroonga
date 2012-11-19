c19c3ce151368960faffe29b3c6a4196d977df96
Kouhei Sutou <kou@clear-code.com>
2012-11-19 17:01:51 +0900

    mysql56: support timestamp
    refs #1547

--- lib/mrn_multiple_column_key_codec.cpp
+++ lib/mrn_multiple_column_key_codec.cpp
@@ -276,10 +276,9 @@ namespace mrn {
       break;
 #ifdef MRN_HAVE_MYSQL_TYPE_TIMESTAMP2
     case MYSQL_TYPE_TIMESTAMP2:
-      // TODO
       DBUG_PRINT("info", ("mroonga: MYSQL_TYPE_TIMESTAMP2"));
-      *data_type = TYPE_LONG_LONG_NUMBER;
-      *data_size = 8;
+      *data_type = TYPE_BYTE_SEQUENCE;
+      *data_size = key_part->length;
       break;
 #endif
 #ifdef MRN_HAVE_MYSQL_TYPE_DATETIME2
