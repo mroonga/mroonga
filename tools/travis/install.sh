7ccd3c3f7fe5622d95ec9387761bf9df843b9d51
Kouhei Sutou <kou@clear-code.com>
2013-03-25 23:05:03 +0900

    travis: use packaged groonga and groonga-normalizer-mysql

--- tools/travis/install.sh
+++ tools/travis/install.sh
@@ -19,8 +19,8 @@
 # set -x
 set -e
 
-export GROONGA_MASTER=yes
-export GROONGA_NORMALIZER_MYSQL_MASTER=yes
+# export GROONGA_MASTER=yes
+# export GROONGA_NORMALIZER_MYSQL_MASTER=yes
 
 curl https://raw.github.com/groonga/groonga/master/data/travis/setup.sh | sh
 curl https://raw.github.com/groonga/groonga-normalizer-mysql/master/data/travis/setup.sh | sh
