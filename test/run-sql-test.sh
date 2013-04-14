789bc35600c57f18fa1554771410c9634404ec11
Kouhei Sutou <kou@clear-code.com>
2013-04-14 23:33:43 +0900

    test: add license header

--- test/run-sql-test.sh
+++ test/run-sql-test.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+#
+# Copyright(C) 2010  Tetsuro IKEDA
+# Copyright(C) 2010-2013  Kouhei Sutou <kou@clear-code.com>
+# Copyright(C) 2011  Kazuhiko
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 export BASE_DIR="$(cd $(dirname $0); pwd)"
 top_dir="$BASE_DIR/.."
