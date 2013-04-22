938685f143e8391db0ab9bb53f1de28d565f36df
Kouhei Sutou <kou@clear-code.com>
2013-04-22 15:26:02 +0900

    travis: use groonga master
    It is required by groogna-normalizer-mysql.

--- tools/travis/install.sh
+++ tools/travis/install.sh
@@ -19,7 +19,7 @@
 # set -x
 set -e
 
-# export GROONGA_MASTER=yes
+export GROONGA_MASTER=yes
 export GROONGA_NORMALIZER_MYSQL_MASTER=yes
 
 curl https://raw.github.com/groonga/groonga/master/data/travis/setup.sh | sh
