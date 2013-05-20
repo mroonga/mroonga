594fff1c92e4e312caaa95672626f920a1a04a7a
Kouhei Sutou <kou@clear-code.com>
2013-05-20 14:46:32 +0900

    travis: use groonga-normalizer-mysql at master
    We can't combine GROONGA_MASTER=yes and GROONGA_NORMALIZER_MYSQL_MASTER=no.

--- tools/travis/install.sh
+++ tools/travis/install.sh
@@ -20,7 +20,7 @@ set -x
 set -e
 
 export GROONGA_MASTER=yes
-# export GROONGA_NORMALIZER_MYSQL_MASTER=yes
+export GROONGA_NORMALIZER_MYSQL_MASTER=yes
 
 curl https://raw.github.com/groonga/groonga/master/data/travis/setup.sh | sh
 curl https://raw.github.com/groonga/groonga-normalizer-mysql/master/data/travis/setup.sh | sh
