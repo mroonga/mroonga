a09de0d7cb06f999c89af3ffeb434db5d913f8d6
HAYASHI Kentaro <hayashi@clear-code.com>
2013-05-24 11:11:35 +0900

    packages: support Debian 8.0 (jessie)

--- packages/check-utility.sh
+++ packages/check-utility.sh
@@ -80,7 +80,7 @@ setup_rpm_architectures ()
 setup_codes ()
 {
     if [ -z "$CODES" ]; then
-	CODES="squeeze wheezy unstable lucid precise quantal raring"
+	CODES="squeeze wheezy jessie unstable lucid precise quantal raring"
     fi
 }
 setup_deb_architectures ()
