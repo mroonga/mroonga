13d20748483a3116a6d59101c546124b1649acf0
HAYASHI Kentaro <hayashi@clear-code.com>
2013-11-20 10:49:29 +0900

    doc: don't use capitalized notation for man page
    it breaks 'make dist', sorry.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -238,6 +238,6 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'Mroonga', u'Mroonga Documentation',
+    ('index', 'mroonga', u'Mroonga Documentation',
      [u'Mroonga Project'], 1)
 ]
