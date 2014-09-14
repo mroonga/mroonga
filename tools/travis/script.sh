332406da8e9d2210007418ef428b8782550d1c97
Kouhei Sutou <kou@clear-code.com>
2014-09-14 11:43:06 +0900

    travis: disable concurrency build for MariaDB bundled build
        make[3]: warning: jobserver unavailable: using -j1.  Add `+' to parent make rule.

--- tools/travis/script.sh
+++ tools/travis/script.sh
@@ -31,10 +31,14 @@ else
     . "${top_dir}/config.sh"
 fi
 
-n_processors="$(grep '^processor' /proc/cpuinfo | wc -l)"
-max_n_processors=8
-if (( $n_processors > $max_n_processors )); then
-    n_processors=$max_n_processors
+if [ "${MROONGA_BUNDLED}" = "yes" ]; then
+    n_processors=1
+else
+    n_processors="$(grep '^processor' /proc/cpuinfo | wc -l)"
+    max_n_processors=8
+    if (( $n_processors > $max_n_processors )); then
+	n_processors=$max_n_processors
+    fi
 fi
 
 build()
