e22487b4f1c7ab900df8a0ec465b674b34b0cda1
Kouhei Sutou <kou@clear-code.com>
2015-03-14 23:42:42 +0900

    mysql57: include mrn_variable.hpp

--- udf/mrn_udf_escape.cpp
+++ udf/mrn_udf_escape.cpp
@@ -1,6 +1,6 @@
 /* -*- c-basic-offset: 2; indent-tabs-mode: nil -*- */
 /*
-  Copyright(C) 2013 Kouhei Sutou <kou@clear-code.com>
+  Copyright(C) 2013-2015 Kouhei Sutou <kou@clear-code.com>
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -22,6 +22,7 @@
 #include <mrn_path_mapper.hpp>
 #include <mrn_windows.hpp>
 #include <mrn_macro.hpp>
+#include <mrn_variables.hpp>
 
 MRN_BEGIN_DECLS
 
