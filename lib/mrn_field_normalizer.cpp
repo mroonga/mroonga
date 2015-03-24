d5e854dd6d42a6768dfc7e2ec7315df9c9dc7786
Kouhei Sutou <kou@clear-code.com>
2015-03-24 23:26:04 +0900

    Support utf8_unicode_520_ci and utf8mb4_unicode_520_ci
    It requires groonga-normalizer-mysql 1.0.9. It's not released yet.
    
    TODO:
    
      * Update required groonga-normalizer-mysql version after new
        groonga-normalizer-mysql is released.
      * Release groonga-normalizer-mysql before Mroonga.

--- lib/mrn_field_normalizer.cpp
+++ lib/mrn_field_normalizer.cpp
@@ -114,6 +114,9 @@ namespace mrn {
     } else if ((strcmp(charset_info->name, "utf8_unicode_ci") == 0) ||
                (strcmp(charset_info->name, "utf8mb4_unicode_ci") == 0)) {
       normalizer_name = "NormalizerMySQLUnicodeCI";
+    } else if ((strcmp(charset_info->name, "utf8_unicode_520_ci") == 0) ||
+               (strcmp(charset_info->name, "utf8mb4_unicode_520_ci") == 0)) {
+      normalizer_name = "NormalizerMySQLUnicode520CI";
     }
 
     grn_obj *normalizer = NULL;
