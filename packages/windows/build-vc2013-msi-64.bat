51a2f6e2df05edc36e95b4f1ce46664b03c8ced6
Kouhei Sutou <kou@clear-code.com>
2015-10-19 11:21:10 +0900

    Add year

--- packages/windows/build-vc2013-msi-64.bat
+++ packages/windows/build-vc2013-msi-64.bat
@@ -1,7 +1,7 @@
 rmdir /S /Q build-vc2013-msi-64
 mkdir build-vc2013-msi-64
 cd build-vc2013-msi-64
-cmake ..\source -G "Visual Studio 12 Win64" > config.log
+cmake ..\source -G "Visual Studio 12 2013 Win64" > config.log
 cmake --build . --config RelWithDebInfo > build.log
 cmake --build . --config RelWithDebInfo --target msi > msi.log
 move *.msi ..\
