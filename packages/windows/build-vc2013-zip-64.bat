6ebd055cd1e39aeecc6089cd66f1fd31bc235b43
Kouhei Sutou <kou@clear-code.com>
2016-02-27 22:44:46 +0900

    package windows: fold a long line

--- packages/windows/build-vc2013-zip-64.bat
+++ packages/windows/build-vc2013-zip-64.bat
@@ -1,7 +1,11 @@
 rmdir /S /Q build-vc2013-zip-64
 mkdir build-vc2013-zip-64
 cd build-vc2013-zip-64
-cmake ..\source -G "Visual Studio 12 2013 Win64" -DMRN_GROONGA_EMBED=OFF -DMRN_GROONGA_NORMALIZER_MYSQL_EMBED=OFF -DGRN_WITH_BUNDLED_MECAB=ON > config.log
+cmake ..\source -G "Visual Studio 12 2013 Win64" ^
+  -DMRN_GROONGA_EMBED=OFF ^
+  -DMRN_GROONGA_NORMALIZER_MYSQL_EMBED=OFF ^
+  -DGRN_WITH_BUNDLED_MECAB=ON ^
+  > config.log
 cmake --build . --config RelWithDebInfo > build.log
 cmake --build . --config RelWithDebInfo --target package > zip.log
 move *.zip ..\
