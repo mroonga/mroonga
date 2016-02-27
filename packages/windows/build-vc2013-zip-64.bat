04977756af46b3ed95c75c83336ede6d0730d17e
Kouhei Sutou <kou@clear-code.com>
2016-02-27 22:45:29 +0900

    package windows: use bundled LZ4

--- packages/windows/build-vc2013-zip-64.bat
+++ packages/windows/build-vc2013-zip-64.bat
@@ -4,6 +4,7 @@ cd build-vc2013-zip-64
 cmake ..\source -G "Visual Studio 12 2013 Win64" ^
   -DMRN_GROONGA_EMBED=OFF ^
   -DMRN_GROONGA_NORMALIZER_MYSQL_EMBED=OFF ^
+  -DGRN_WITH_BUNDLED_LZ4=ON ^
   -DGRN_WITH_BUNDLED_MECAB=ON ^
   > config.log
 cmake --build . --config RelWithDebInfo > build.log
--- packages/windows/build-vc2015-zip-32.bat
+++ packages/windows/build-vc2015-zip-32.bat
@@ -4,6 +4,7 @@ cd build-vc2015-zip-32
 cmake ..\source -G "Visual Studio 14 2015" ^
   -DMRN_GROONGA_EMBED=OFF ^
   -DMRN_GROONGA_NORMALIZER_MYSQL_EMBED=OFF ^
+  -DGRN_WITH_BUNDLED_LZ4=ON ^
   -DGRN_WITH_BUNDLED_MECAB=ON ^
   > config.log
 cmake --build . --config RelWithDebInfo > build.log
