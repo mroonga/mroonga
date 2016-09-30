7ec49c1b8a0ac85a2450e4c240d0fc13cf8ab7cd
Kouhei Sutou <kou@clear-code.com>
2016-09-30 23:32:24 +0900

    travis: fix package name

--- tools/travis/install.sh
+++ tools/travis/install.sh
@@ -126,10 +126,10 @@ else
     percona-server-*)
       setup_percona_apt
       set -x
-      sudo apt-get -y build-dep percona-server-${series}
+      sudo apt-get -y build-dep percona-server-server-${series}
       sudo apt-get -y install \
-           percona-server-${series} percona-server-test-${series}
-      apt-get source percona-server-${series}
+           percona-server-server-${series} percona-server-test-${series}
+      apt-get source percona-server-server-${series}
       ln -s $(find . -maxdepth 1 -type d | sort | tail -1) mysql
       ;;
   esac
