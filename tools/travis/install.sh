298df6328461519300e83fbe6be901ef3d761f4e
Kouhei Sutou <kou@clear-code.com>
2016-09-30 23:02:33 +0900

    travis: fix regex

--- tools/travis/install.sh
+++ tools/travis/install.sh
@@ -24,8 +24,8 @@ set -e
 
 mariadb_download_base=http://mirror.jmu.edu/pub/mariadb
 
-version=$(echo "$MYSQL_VERSION" | sed -e 's/^\(mysql\|mariadb\|percona-server\)-//')
-series=$(echo "$version" | sed -e 's/\.[0-9]\.[0-9]*\(-\?[a-z]*\)\?$//g')
+version=$(echo "$MYSQL_VERSION" | sed -r -e 's/^(mysql|mariadb|percona-server)-//')
+series=$(echo "$version" | sed -r -e 's/^([0-9]+\.[0-9]+).*$/\1/g')
 
 setup_mariadb_apt()
 {
