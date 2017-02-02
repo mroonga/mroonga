e4f63e2fddf9f9245c25be4f6179d3ffe70173a6
Kouhei Sutou <kou@clear-code.com>
2017-02-02 11:10:59 +0900

    storage: support updating row its table has primary key with ROW binlog format
    GitHub: fix #130
    
    Reported by kitora. Thanks!!!

--- ha_mroonga.cpp
+++ ha_mroonga.cpp
@@ -6465,30 +6465,42 @@ int ha_mroonga::storage_update_row(const uchar *old_data, uchar *new_data)
       if (error)
         goto err;
 
+      bool is_pkey = false;
       bool on_duplicate_key_update =
         (inserting_with_update && ignoring_duplicated_key);
-      if (!on_duplicate_key_update && pkey_info) {
-        bool have_pkey = false;
+      if (pkey_info && on_duplicate_key_update) {
         for (j = 0; j < KEY_N_KEY_PARTS(pkey_info); j++) {
           Field *pkey_field = pkey_info->key_part[j].field;
           if (strcmp(pkey_field->field_name, column_name.c_str()) == 0) {
-            if (!replacing_) {
-              char message[MRN_BUFFER_SIZE];
-              snprintf(message, MRN_BUFFER_SIZE,
-                       "data truncated for primary key column: <%s>",
-                       column_name.c_str());
-              push_warning(thd, MRN_SEVERITY_WARNING,
-                           WARN_DATA_TRUNCATED, message);
-            }
-            have_pkey = true;
+            is_pkey = true;
+            break;
           }
-        }
-        if (have_pkey) {
-          continue;
         }
       }
 
       generic_store_bulk(field, &colbuf);
+      if (is_pkey) {
+        char key[GRN_TABLE_MAX_KEY_SIZE];
+        int key_size;
+        key_size = grn_table_get_key(ctx,
+                                     grn_table,
+                                     record_id,
+                                     &key,
+                                     GRN_TABLE_MAX_KEY_SIZE);
+        bool is_same_value =
+          (GRN_BULK_VSIZE(&colbuf) == key_size &&
+           memcmp(key, GRN_BULK_HEAD(&colbuf), key_size) == 0);
+        if (!is_same_value && replacing_) {
+          char message[MRN_BUFFER_SIZE];
+          snprintf(message, MRN_BUFFER_SIZE,
+                   "data truncated for primary key column: <%s>",
+                   column_name.c_str());
+          push_warning(thd, MRN_SEVERITY_WARNING,
+                       WARN_DATA_TRUNCATED, message);
+        }
+        continue;
+      }
+
       grn_obj_set_value(ctx, grn_columns[i], record_id, &colbuf, GRN_OBJ_SET);
       if (ctx->rc) {
         grn_obj_unlink(ctx, &colbuf);
