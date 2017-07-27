ee06d23c560aba1a5875ed633dcec95503b11edf
Kouhei Sutou <kou@clear-code.com>
2017-07-27 11:27:51 +0900

    Add mroonga_highlight_html UDF

--- data/uninstall.sql
+++ data/uninstall.sql
@@ -4,6 +4,7 @@ DROP FUNCTION IF EXISTS mroonga_command;
 DROP FUNCTION IF EXISTS mroonga_escape;
 DROP FUNCTION IF EXISTS mroonga_snippet_html;
 DROP FUNCTION IF EXISTS mroonga_normalize;
+DROP FUNCTION IF EXISTS mroonga_highlight_html;
 
 UNINSTALL PLUGIN Mroonga;
 
