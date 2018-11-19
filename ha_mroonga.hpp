9f1c6c202fd9db6847a90ddceeb33fedd1d9567f
Kouhei Sutou <kou@clear-code.com>
2018-11-19 15:08:57 +0900

    Simplify

--- ha_mroonga.hpp
+++ ha_mroonga.hpp
@@ -234,9 +234,11 @@ extern "C" {
 #endif
 
 /* Note that MariaDB 10.2.2 and older MariaDB 10.2 series are not supported since 8.07 */
-#if (MYSQL_VERSION_ID >= 100136 && defined(MRN_MARIADB_P)) || \
-  (MYSQL_VERSION_ID >= 100037 && MYSQL_VERSION_ID < 100100 && defined(MRN_MARIADB_P))
-#  define MRN_FOREIGN_KEY_USE_METHOD_ENUM
+#ifdef MRN_MARIADB_P
+#  if (MYSQL_VERSION_ID >= 100037 && MYSQL_VERSION_ID < 100100) || \
+      (MYSQL_VERSION_ID >= 100136)
+#    define MRN_FOREIGN_KEY_USE_METHOD_ENUM
+#  endif
 #endif
 
 #if MYSQL_VERSION_ID < 50706 || defined(MRN_MARIADB_P)
