180da582128ea664d4b6e19c558c5aa7cca1230f
Kentaro Hayashi <hayashi@clear-code.com>
2019-06-27 16:53:13 +0900

    packages yum: skip to build MySQL 8 on i386
    Required scl package is not provided.

--- packages/yum/build-in-vm.sh
+++ packages/yum/build-in-vm.sh
@@ -73,6 +73,9 @@ for mysql_variant in ${MYSQL_VARIANTS}; do
       if [ ${centos_version} = 7 -a ${architecture} = i386 ]; then
         continue
       fi
+      if [ ${centos_version} = 6 -a ${architecture} = i386 -a ${mysql_variant} = "mysql80-community" ]; then
+        continue
+      fi
       id=centos-${centos_version}-${architecture}
       vagrant up ${id}
       build_status=$?
