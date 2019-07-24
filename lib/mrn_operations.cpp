ac549b53e45c7666db890e64dbbab26ca867b0bc
Sutou Kouhei <kou@clear-code.com>
2019-07-25 06:37:32 +0900

    auto recover: add more information to error message
    We can't recover automatically when Mroonga is crashed while
    updating. This adds more information to error message for the case.

--- lib/mrn_operations.cpp
+++ lib/mrn_operations.cpp
@@ -319,9 +319,15 @@ namespace mrn {
         }
       } else if (strcmp(GRN_TEXT_VALUE(&text_buffer_), "update") == 0) {
         error = HA_ERR_CRASHED_ON_USAGE;
-        my_message(error,
-                   "mroonga: repair: can't recover from crash while updating",
-                   MYF(0));
+        char error_message[MRN_MESSAGE_BUFFER_SIZE];
+        snprintf(error_message, MRN_MESSAGE_BUFFER_SIZE,
+                 "mroonga: repair: can't recover from crash while updating: "
+                 "[%u]: <%.*s>[%u][%s]",
+                 id,
+                 static_cast<int>(table_name_size), table_name,
+                 record_id,
+                 GRN_TEXT_VALUE(&text_buffer_));
+        my_message(error, error_message, MYF(0));
         break;
       } else {
         error = HA_ERR_CRASHED_ON_USAGE;
