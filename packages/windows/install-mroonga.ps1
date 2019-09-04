c829034a4114184ad6e82a6e29816a9b9969f344
Kentaro Hayashi <hayashi@clear-code.com>
2019-09-04 16:49:39 +0900

    appveyor: do not use fixed timeout

--- packages/windows/install-mroonga.ps1
+++ packages/windows/install-mroonga.ps1
@@ -3,20 +3,52 @@ Param(
   [Parameter(mandatory=$true)][String]$platform
 )
 
-function Wait-UntilRunning($cmdName) {
+function Run-MySQL {
+  Write-Output("Start mysqld.exe")
+  Start-Process .\bin\mysqld.exe -ArgumentList "--console" -RedirectStandardError ..\mysqld.txt
+  $Waiting = $TRUE
   do
   {
-    $Running = Get-Process $cmdName -ErrorAction SilentlyContinue
-    Start-Sleep -m 500
-  } while (!$Running)
+    if (Test-Path ..\mysqld.txt) {
+      $Version = Get-Content ..\mysqld.txt | Select-String -Pattern "^Version:"
+      if ($Version) {
+        $Waiting = $FALSE
+      }
+    }
+    Start-Sleep -s 1
+  } while ($Waiting)
 }
 
-function Wait-UntilTerminate($cmdName) {
+function Run-MySQLInstallDB {
+  Write-Output("Start mysql_install_db.exe")
+  Start-Process .\bin\mysql_install_db.exe -RedirectStandardOutput ..\install.txt
+  $Waiting = $TRUE
   do
   {
-    $Running = Get-Process $cmdName -ErrorAction SilentlyContinue
-    Start-Sleep -m 500
-  } while ($Running)
+    if (Test-Path ..\install.txt) {
+      $Successful = Get-Content ..\install.txt | Select-String -Pattern "successful"
+      if ($Successful) {
+        $Waiting = $FALSE
+      }
+    }
+    Start-Sleep -s 1
+  } while ($Waiting)
+}
+
+function Run-MySQLAdmin {
+  Write-Output("Shutdown mysqld.exe")
+  Start-Process .\bin\mysqladmin.exe -ArgumentList "-uroot shutdown"
+  $Waiting = $TRUE
+  do
+  {
+    if (Test-Path ..\mysqld.txt) {
+      $Complete = Get-Content ..\mysqld.txt | Select-String -Pattern "Shutdown complete"
+      if ($Complete) {
+        $Waiting = $FALSE
+      }
+    }
+    Start-Sleep -s 1
+  } while ($Waiting)
 }
 
 function Install-Mroonga($mariadbVer, $arch, $installSqlDir) {
@@ -25,17 +57,12 @@ function Install-Mroonga($mariadbVer, $arch, $installSqlDir) {
   if ("$mariadbVer" -eq "10.4.7") {
     Write-Output("Clean data directory")
     Remove-Item data -Recurse
-    Start-Process .\bin\mysql_install_db.exe
-    Start-Sleep -s 10
+    Run-MySQLInstallDB
   }
-  Write-Output("Start mysqld.exe")
-  Start-Process .\bin\mysqld.exe
-  Start-Sleep -s 10
+  Run-MySQL
   Write-Output("Execute install.sql")
   Get-Content "$installSqlDir\install.sql" | .\bin\mysql.exe -uroot
-  Write-Output("Shutdown mysqld.exe")
-  Start-Process .\bin\mysqladmin.exe -ArgumentList "-uroot shutdown"
-  Start-Sleep -s 10
+  Run-MySQLAdmin
   cd ..
   Write-Output("Finished to install Mroonga")
 }
