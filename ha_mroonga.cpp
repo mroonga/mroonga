27e58cc09bd66c5c224f1f4ee4d0904889f65861
Horimoto Yasuhiro <horimoto@clear-code.com>
2020-08-11 14:55:58 +0900

    Add support for mariadb-server 10.5.5
    GitHub: fix GH-333 Reported by tomop-tg. Thanks!!!
    
    We use ha_drop_table instead of ha_delete_table.
    Because ha_delete_table is deleted since mariadb-server 10.5.5
    by below commit.
    
    https://github.com/MariaDB/server/commit/c55c292832e2776d37e06c43174ac006e00143a2

--- ha_mroonga.cpp
+++ ha_mroonga.cpp
@@ -5671,7 +5671,13 @@ int ha_mroonga::wrapper_delete_table(const char *name,
     DBUG_RETURN(HA_ERR_OUT_OF_MEM);
   }
 
-#ifdef MRN_HANDLER_DELETE_TABLE_HAVE_TABLE_DEFINITION
+#ifdef MRN_HANDLER_DELETE_TABLE_HAVE_HTON_DROP_TABLE
+  if (ha_check_if_updates_are_ignored(ha_thd(), wrap_handlerton, "DROP")) {
+    error = 0;
+  } else {
+    error = wrap_handlerton->drop_table(wrap_handlerton, name);
+  }
+#elif defined(MRN_HANDLER_DELETE_TABLE_HAVE_TABLE_DEFINITION)
   error = hnd->ha_delete_table(name, table_def);
 #else
   error = hnd->ha_delete_table(name);
