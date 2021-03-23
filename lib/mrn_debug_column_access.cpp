daa6960ab7aaf1aa23ab699dc93536830f8770cf
Horimoto Yasuhiro <horimoto@clear-code.com>
2021-03-23 16:54:44 +0900

    Don't use raw bitmap (#395)
    Because the following patch changes function prototypes for
    "dbug_tmp_restore_column_maps" and "dbug_tmp_use_all_columns".
    
    See: https://github.com/MariaDB/server/commit/21809f9a450df1bc44cef36377f96b516ac4a9ae#diff-ef35bee880ab249447fa18d023c6e658329bb1f02d486742b95c68e8075fce2bR2990
    
    Co-authored-by: Sutou Kouhei <kou@clear-code.com>

--- lib/mrn_debug_column_access.cpp
+++ lib/mrn_debug_column_access.cpp
@@ -1,6 +1,7 @@
 /* -*- c-basic-offset: 2 -*- */
 /*
   Copyright(C) 2012-2018 Kouhei Sutou <kou@clear-code.com>
+  Copyright(C) 2021 Horimoto Yasuhiro <horimoto@clear-code.com>
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -21,17 +22,25 @@
 #include <table.h>
 
 namespace mrn {
-  DebugColumnAccess::DebugColumnAccess(TABLE *table, MY_BITMAP *bitmap)
+  DebugColumnAccess::DebugColumnAccess(TABLE *table, MY_BITMAP **bitmap)
     : table_(table),
       bitmap_(bitmap) {
 #ifndef DBUG_OFF
+#  ifdef MRN_DBUG_TMP_USE_BITMAP_PP
     map_ = dbug_tmp_use_all_columns(table_, bitmap_);
+#  else
+    map_ = dbug_tmp_use_all_columns(table_, *bitmap_);
+#  endif
 #endif
   }
 
   DebugColumnAccess::~DebugColumnAccess() {
 #ifndef DBUG_OFF
+#  ifdef MRN_DBUG_TMP_USE_BITMAP_PP
     dbug_tmp_restore_column_map(bitmap_, map_);
+#  else
+    dbug_tmp_restore_column_map(*bitmap_, map_);
+#  endif
 #endif
   }
 }
--- lib/mrn_debug_column_access.hpp
+++ lib/mrn_debug_column_access.hpp
@@ -1,6 +1,7 @@
 /* -*- c-basic-offset: 2 -*- */
 /*
   Copyright(C) 2012-2018 Kouhei Sutou <kou@clear-code.com>
+  Copyright(C) 2021 Horimoto Yasuhiro <horimoto@clear-code.com>
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -23,15 +24,21 @@
 #include <mrn_mysql.h>
 #include <my_bitmap.h>
 
+#include "mrn_mysql_compat.h"
+
 namespace mrn {
   class DebugColumnAccess {
     TABLE *table_;
-    MY_BITMAP *bitmap_;
+    MY_BITMAP **bitmap_;
 #ifndef DBUG_OFF
+#  ifdef MRN_DBUG_TMP_USE_BITMAP_PP
+    MY_BITMAP *map_;
+#  else
     my_bitmap_map *map_;
+#  endif
 #endif
   public:
-    DebugColumnAccess(TABLE *table, MY_BITMAP *bitmap);
+    DebugColumnAccess(TABLE *table, MY_BITMAP **bitmap);
     ~DebugColumnAccess();
   };
 }
