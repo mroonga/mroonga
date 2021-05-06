3d5a78c712ea60c6df4834410e3efb4f7a073663
Sutou Kouhei <kou@clear-code.com>
2021-05-06 16:26:47 +0900

    mysql80 test: don't use 'INT(N)'
    It's deprecated since MySQL 8.0.

--- tools/travis/script.sh
+++ tools/travis/script.sh
@@ -123,7 +123,6 @@ run_sql_test()
       rm -f fulltext/t/found_rows.test
       rm -f geometry/strict_sql_mode/t/bulk_insert_null.test
       rm -f index/unique/t/varchar.test
-      rm -f index/unique/t/multiple_update.test
       rm -f like/t/unicode_ci.test
       rm -f select/empty_key/t/where_not_equal.test
       rm -f replace/t/select_varchar.test
