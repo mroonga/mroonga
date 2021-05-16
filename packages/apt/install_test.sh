65fef5f13f7d206b10d1dc61ea0088884b616e91
Horimoto Yasuhiro <horimoto@clear-code.com>
2021-05-16 20:50:03 +0900

    Use apt instead apt-get
    Because Ubuntu recommend using apt instead apt-get.

--- packages/apt/install_test.sh
+++ packages/apt/install_test.sh
@@ -5,12 +5,12 @@ set -exu
 package=$1
 
 sudo apt update
-sudo apt-get install -y -V software-properties-common lsb-release
+sudo apt install -y -V software-properties-common lsb-release
 sudo add-apt-repository -y universe
 sudo add-apt-repository \
      "deb http://security.ubuntu.com/ubuntu $(lsb_release --short --codename)-security main restricted"
 
 sudo add-apt-repository -y ppa:groonga/ppa
-sudo apt-get update
+sudo apt update
 
 sudo apt install -V -y ${package}
