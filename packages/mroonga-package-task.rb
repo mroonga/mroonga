ef5e12d1226f3c2d56617366356f65ba985a2c91
Horimoto Yasuhiro <horimoto@clear-code.com>
2021-05-31 18:13:08 +0900

    package ubuntu: package name change to ubuntu's package name (#417)
    Mroonga's package name is changed  by the following commit.
    https://github.com/mroonga/mroonga/commit/6bd846f2136fd7c657f232212cfc4759f9dc7e59
    
    However, Ubuntu's MySQL package is mariadb-server-10.x and mysql-server-5.x.
    Therefore, we need to convert Mroonga's package name to Ubuntu's package name.
    Otherwise, we can't get version for MySQL and MariaDB for Ubuntu.

--- packages/mroonga-package-task.rb
+++ packages/mroonga-package-task.rb
@@ -89,6 +89,9 @@ class MroongaPackageTask < PackagesGroongaOrgPackageTask
     cache_key = [code_name, package_name]
     version = @ubuntu_package_versions[cache_key]
     return version if version
+    # "mariadb-10.5" -> "mariadb-server-10.5"
+    # "mysql-5.7" -> "mysql-server-5.7"
+    ubuntu_package_name = package_name.sub(/-/, "-server-")
     source_names = [code_name, "#{code_name}-updates"]
     source_names.each do |source_name|
       all_packages_url =
@@ -96,7 +99,7 @@ class MroongaPackageTask < PackagesGroongaOrgPackageTask
       URI.open(all_packages_url) do |all_packages|
         all_packages.each_line do |line|
           case line
-          when /\A#{Regexp.escape(package_name)} \((.+?)[\s)]/o
+          when /\A#{Regexp.escape(ubuntu_package_name)} \((.+?)[\s)]/o
             version = $1
           end
         end
