ce73eba9d7e9fbae00cf866947b199232efa4f1d
Horimoto Yasuhiro <horimoto@clear-code.com>
2021-10-21 16:05:16 +0900

    Don't use init_alloc_root()
    Because init_alloc_root() is removed since MySQL 8.0.27.

--- mrn_mysql_compat.h
+++ mrn_mysql_compat.h
@@ -215,9 +215,12 @@
 #elif defined(MRN_MARIADB_P) && MYSQL_VERSION_ID >= 100000
 #  define mrn_init_alloc_root(root, name, block_size, pre_alloc_size, flags) \
   init_alloc_root(root, block_size, pre_alloc_size, flags)
-#elif MYSQL_VERSION_ID >= 50706
+#elif (MYSQL_VERSION_ID >= 50706) && (MYSQL_VERSION_ID < 80027)
 #  define mrn_init_alloc_root(root, name, block_size, pre_alloc_size, flags) \
   init_alloc_root(mrn_memory_key, root, block_size, pre_alloc_size)
+#elif !defined(MRN_MARIADB_P) && (MYSQL_VERSION_ID >= 80027)
+#  define mrn_init_alloc_root(root, name, block_size, pre_alloc_size, flags) \
+  ::new ((void *)root) MEM_ROOT(mrn_memory_key, block_size)
 #else
 #  define mrn_init_alloc_root(root, name, block_size, pre_alloc_size, flags) \
   init_alloc_root(root, block_size, pre_alloc_size)
