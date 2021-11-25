caaa78e39f3d743086cf8ee430aa99f6135e17c3
Sutou Kouhei <kou@clear-code.com>
2021-11-25 11:13:17 +0900

    ci packages yum: check_ha_mroonga_so.inc is used for loading function

--- packages/yum/test.sh
+++ packages/yum/test.sh
@@ -100,6 +100,8 @@ cd /usr/share/mysql-test/
 sudo rm -rf plugin
 sudo mkdir -p plugin
 sudo cp -a /vagrant/mysql-test/mroonga/ plugin/
+sed -i'' -e "s/ha_mroonga\\.so/${ha_mroonga_so}/g" \
+  plugin/mroonga/include/mroonga/check_ha_mroonga_so.inc
 sed -i'' -e "s/\$HA_MROONGA_SO/${ha_mroonga_so}/g" \
   plugin/mroonga/include/mroonga/have_mroonga.opt
 
