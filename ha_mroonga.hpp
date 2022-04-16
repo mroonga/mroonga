a858168789b0d4d945293bd9d24e200fe3051930
Sutou Kouhei <kou@clear-code.com>
2022-04-16 15:44:49 +0900

    mariadb10.5: MariaDB 10.5.10 removed HA_EXTRA_FAKE_START_STMT

--- ha_mroonga.hpp
+++ ha_mroonga.hpp
@@ -102,7 +102,7 @@ extern "C" {
 #endif
 
 #if (defined(MRN_MARIADB_P) &&                                  \
-     (MYSQL_VERSION_ID >= 100224 && MYSQL_VERSION_ID < 100600))
+     (MYSQL_VERSION_ID >= 100224 && MYSQL_VERSION_ID < 100510))
 #  define MRN_HAVE_HA_EXTRA_FAKE_START_STMT
 #endif
 
