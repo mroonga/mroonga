5296dcbd59cbcec50b2f517ad506871b2811dc17
Sutou Kouhei <kou@clear-code.com>
2022-10-07 16:27:45 +0900

    ci windows: replace more /Zi

--- build/msvc_ccache.cmake
+++ build/msvc_ccache.cmake
@@ -1,10 +1,20 @@
 # ccache doesn't work with /Zi.
 # See also: https://github.com/ccache/ccache/issues/1040
-string(REPLACE "/Zi" "/Z7" CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
-string(REPLACE "/Zi" "/Z7" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
-string(REPLACE "/Zi" "/Z7"
-  CMAKE_C_FLAGS_RELWITHDEBINFO
-  "${CMAKE_C_FLAGS_RELWITHDEBINFO}")
-string(REPLACE "/Zi" "/Z7"
-  CMAKE_CXX_FLAGS_RELWITHDEBINFO
-  "${CMAKE_CXX_FLAGS_RELWITHDEBINFO}")
+foreach(lang C CXX)
+  foreach(flag_suffix
+      ""
+      "_INIT"
+      "_RELEASE"
+      "_RELEASE_INIT"
+      "_RELWITHDEBINFO"
+      "_RELWITHDEBINFO_INIT"
+      "_DEBUG"
+      "_DEBUG_INIT"
+      "_MINSIZEREL"
+      "_MINSIZEREL_INIT")
+    message("XXX: ${lang}:${flag_suffix}: CMAKE_${lang}_FLAGS${flag_suffix}: ${CMAKE_${lang}_FLAGS${flag_suffix}}")
+    string(REPLACE "/Zi" "/Z7"
+      CMAKE_${lang}_FLAGS${flag_suffix}
+      "${CMAKE_${lang}_FLAGS${flag_suffix}}")
+  endforeach()
+endforeach()
