db79e380f36376ecb920b68d6c42264c4d28b43e
Sutou Kouhei <kou@clear-code.com>
2022-10-07 15:08:18 +0900

    ci windows: replace more /Zi

--- build/msvc_ccache.cmake
+++ build/msvc_ccache.cmake
@@ -1,5 +1,7 @@
 # ccache doesn't work with /Zi.
 # See also: https://github.com/ccache/ccache/issues/1040
+string(REPLACE "/Zi" "/Z7" CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
+string(REPLACE "/Zi" "/Z7" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
 string(REPLACE "/Zi" "/Z7"
   CMAKE_C_FLAGS_RELWITHDEBINFO
   "${CMAKE_C_FLAGS_RELWITHDEBINFO}")
