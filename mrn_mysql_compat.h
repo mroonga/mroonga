cf57f1366a65345bf07185024ae719f6c92c8228
Horimoto Yasuhiro <horimoto@clear-code.com>
2024-08-14 11:10:43 +0900

    Concat same conditions

--- mrn_mysql_compat.h
+++ mrn_mysql_compat.h
@@ -972,21 +972,13 @@ typedef uint mrn_srid;
   (query_tables->get_table_name().str)
 #  define MRN_GET_TABLE_LENGTH(query_tables)                       \
   (query_tables->get_table_name().length)
+#  define MRN_GET_DB_NAME(table_list)                              \
+  (table_list->get_db_name().str)
 #else
 #  define MRN_GET_TABLE_NAME(query_tables)                         \
   (query_tables->get_table_name())
 #  define MRN_GET_TABLE_LENGTH(query_tables)                       \
   (strlen(query_tables->get_table_name()))
-#endif
-
-#if defined(MRN_MARIADB_P) &&                                      \
-    ((MYSQL_VERSION_ID >= 100526 && MYSQL_VERSION_ID < 100600) ||  \
-     (MYSQL_VERSION_ID >= 100619 && MYSQL_VERSION_ID < 100700) ||  \
-     (MYSQL_VERSION_ID >= 101109 && MYSQL_VERSION_ID < 101200))
-
-#  define MRN_GET_DB_NAME(table_list)                              \
-  (table_list->get_db_name().str)
-#else
 #  define MRN_GET_DB_NAME(table_list)                              \
   (table_list->get_db_name())
 #endif
