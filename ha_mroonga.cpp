2951acef7cd888adb4a2d84fa8bca01e765b6a6e
otegami <otegami@clear-code.com>
2024-10-09 12:27:29 +0900

    bit: update index for NULL with `0`(`b'0'`) to support index_column_diff
    GitHub: GH-789
    
    The current implementation ignores `NULL`. It means that we don't update index for `NULL`.
    But it causes `index_column_diff` false positive. `BIT` with `NULL` is processed as `0` in Groonga.
    Because Groonga uses the default value for `NULL`. `BIT` value is the binary in MySQL/MariaDB internal.
    `NULL` `BIT` value is `NULL` in MySQL/MariaDB internal. We use `0` not `NULL` for Groonga because Groonga doesn't support `NULL`.
    The `index_column_diff` uses `0` for `NULL` in the expected posting lists. It causes false positive.
    
    If we use `0` instead of ignoring for `NULL`, we can avoid the false positive.
    It also enables that we can search `NULL` column value record by b`0`.
    
    In general, it'll be useful but this is an incompatible change.
    But it'll be acceptable because `NULL` behavior is undefined by definition.

--- ha_mroonga.cpp
+++ ha_mroonga.cpp
@@ -7367,7 +7367,8 @@ int ha_mroonga::storage_write_row(mrn_write_row_buf_t buf)
                              (field->real_type() != MYSQL_TYPE_FLOAT) &&
                              (field->real_type() != MYSQL_TYPE_DOUBLE) &&
                              (field->real_type() != MYSQL_TYPE_ENUM) &&
-                             (field->real_type() != MYSQL_TYPE_SET)))
+                             (field->real_type() != MYSQL_TYPE_SET) &&
+                             (field->real_type() != MYSQL_TYPE_BIT)))
       continue;
 
 #ifdef MRN_SUPPORT_GENERATED_COLUMNS
@@ -12224,7 +12225,10 @@ int ha_mroonga::generic_store_bulk_unsigned_integer(Field* field, grn_obj* buf)
 {
   MRN_DBUG_ENTER_METHOD();
   int error = 0;
-  long long signed_value = field->val_int();
+  long long signed_value = 0;
+  if (!field->is_null()) {
+    signed_value = field->val_int();
+  }
   unsigned long long unsigned_value = *((unsigned long long*)(&signed_value));
   uint32 size = field->pack_length();
   switch (size) {
