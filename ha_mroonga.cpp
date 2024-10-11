cb1e6610b41633da32ddca730fc6bffea4ee6d2b
takuya kodama <otegami@clear-code.com>
2024-10-11 14:25:56 +0900

    timestamp: update index for `NULL` with `0000-00-00 00:00:00` to support `index_column_diff` (#805)
    GitHub: GH-789
    
    The current implementation ignores `NULL`. It means that we don't update
    index for `NULL`. But it causes `index_column_diff` false positive.
    
    `timestamp` with `NULL` is processed as `0` (`0000-00-00 00:00:00` in
    MySQL/MariaDB) in Groonga. Because Groonga uses the default value for
    `NULL`. `index_column_diff` uses `0` for `NULL` in the expected posting
    lists. It causes false positive.
    
    If we use `0`( `0000-00-00 00:00:00` in MySQL/MariaDB) instead of
    ignoring for `NULL`, we can avoid the false positive. It also enables
    that we can search `NULL` column value record by `0000-00-00 00:00:00`.
    In general, it'll be useful but this is an incompatible change. But
    it'll be acceptable because NULL behavior is undefined by definition.
    
    Note: MySQL enables STRICT_ALL_TABLES by default and it rejects
    `0000-00-00 00:00:00.0`.

--- ha_mroonga.cpp
+++ ha_mroonga.cpp
@@ -7360,6 +7360,7 @@ int ha_mroonga::storage_write_row(mrn_write_row_buf_t buf)
                              (field->real_type() != MYSQL_TYPE_YEAR) &&
                              (field->real_type() != MYSQL_TYPE_TIME) &&
                              (field->real_type() != MYSQL_TYPE_DATETIME) &&
+                             (field->real_type() != MYSQL_TYPE_TIMESTAMP) &&
                              (field->real_type() != MYSQL_TYPE_TIME2) &&
                              (field->real_type() != MYSQL_TYPE_DATETIME2) &&
                              (field->real_type() != MYSQL_TYPE_TIMESTAMP2) &&
@@ -12296,9 +12297,13 @@ int ha_mroonga::generic_store_bulk_timestamp(Field* field, grn_obj* buf)
 {
   MRN_DBUG_ENTER_METHOD();
   int error = 0;
-  Field_timestamp* timestamp_field = static_cast<Field_timestamp*>(field);
-  mrn::TimestampFieldValueConverter<Field_timestamp> converter(timestamp_field);
-  int64_t grn_time = converter.convert();
+  int64_t grn_time = 0;
+  if (!field->is_null()) {
+    Field_timestamp* timestamp_field = static_cast<Field_timestamp*>(field);
+    mrn::TimestampFieldValueConverter<Field_timestamp> converter(
+      timestamp_field);
+    grn_time = converter.convert();
+  }
   grn_obj_reinit(ctx, buf, GRN_DB_TIME, 0);
   GRN_TIME_SET(ctx, buf, grn_time);
   DBUG_RETURN(error);
