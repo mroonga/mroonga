93e22b7867ea71bbb79e910cc3b8abd11f893d0e
otegami <otegami@clear-code.com>
2024-10-11 16:29:01 +0900

    Reduce the scope of variables

--- ha_mroonga.cpp
+++ ha_mroonga.cpp
@@ -12496,12 +12496,12 @@ int ha_mroonga::generic_store_bulk_new_date(Field* field, grn_obj* buf)
   MRN_DBUG_ENTER_METHOD();
   int error = 0;
   bool truncated = false;
-  Field_newdate* newdate_field = (Field_newdate*)field;
-  mrn::TimeConverter time_converter;
   long long int time = 0;
   if (!field->is_null()) {
+    Field_newdate* newdate_field = (Field_newdate*)field;
     MYSQL_TIME mysql_date;
     MRN_FIELD_GET_TIME(newdate_field, &mysql_date, current_thd);
+    mrn::TimeConverter time_converter;
     time = time_converter.mysql_time_to_grn_time(&mysql_date, &truncated);
   }
   if (truncated) {
