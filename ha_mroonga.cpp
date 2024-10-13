b687bc6ea83f243e08e49b3a7ff27e03a03dc3fb
otegami <a.s.takuya1026@gmail.com>
2024-10-13 21:33:41 +0800

    decimal: use `0.0`(zero padding depending on scale) for NULL explicitly
    GitHub: GH-789
    
    The current Mroonga ignores `NULL` values for `DECIMAL`.
    This doesn't change the current behavior. This still ignores `NULL`
    values but `NULL` check code is moved to
    `generic_store_bulk_new_decimal()` from `storage_write_row()` like
    other types. `storage_write_row()` ignores empty bulk after
    `generic_store_bulk_new_decimal()` call. So this doesn't change the
    current behavior.
    
    We want to remove `if (field->is_null() && ...) continue` for
    `grn_obj_set_value()` entirely in `storage_write_row()` eventually.
    This is a step for it.

--- ha_mroonga.cpp
+++ ha_mroonga.cpp
@@ -7368,6 +7368,7 @@ int ha_mroonga::storage_write_row(mrn_write_row_buf_t buf)
                              (field->real_type() != MYSQL_TYPE_TIMESTAMP2) &&
                              (field->real_type() != MYSQL_TYPE_FLOAT) &&
                              (field->real_type() != MYSQL_TYPE_DOUBLE) &&
+                             (field->real_type() != MYSQL_TYPE_NEWDECIMAL) &&
                              (field->real_type() != MYSQL_TYPE_ENUM) &&
                              (field->real_type() != MYSQL_TYPE_SET) &&
                              (field->real_type() != MYSQL_TYPE_BIT) &&
@@ -12537,11 +12538,13 @@ int ha_mroonga::generic_store_bulk_new_decimal(Field* field, grn_obj* buf)
 {
   MRN_DBUG_ENTER_METHOD();
   int error = 0;
-  String value;
-  Field_new_decimal* new_decimal_field = (Field_new_decimal*)field;
-  new_decimal_field->val_str(&value, NULL);
   grn_obj_reinit(ctx, buf, GRN_DB_SHORT_TEXT, 0);
-  GRN_TEXT_SET(ctx, buf, value.ptr(), value.length());
+  if (!field->is_null()) {
+    String value;
+    Field_new_decimal* new_decimal_field = (Field_new_decimal*)field;
+    new_decimal_field->val_str(&value, NULL);
+    GRN_TEXT_SET(ctx, buf, value.ptr(), value.length());
+  }
   DBUG_RETURN(error);
 }
 
