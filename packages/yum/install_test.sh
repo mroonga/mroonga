0b264367a256dccec6685ae7a0ff4f4e0091b143
Horimoto Yasuhiro <horimoto@clear-code.com>
2025-01-08 17:14:16 +0900

    Use "

--- packages/yum/install_test.sh
+++ packages/yum/install_test.sh
@@ -2,23 +2,23 @@
 
 set -exu
 
-package=$1
+package="$1"
 
 os_version=$(cat /etc/almalinux-release | cut -f3 -d' ' | cut -f1 -d'.')
-mariadb_version=$(echo ${package} | awk -F "-" '{print $2}')
+mariadb_version=$(echo "${package}" | awk -F "-" '{print $2}')
 
 cat > /etc/yum.repos.d/MariaDB.repo <<EOF
 [mariadb]
 name = MariaDB
-baseurl = http://yum.mariadb.org/${mariadb_version}/centos${os_version}-amd64
+baseurl = "http://yum.mariadb.org/${mariadb_version}/centos${os_version}-amd64"
 gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1
 EOF
 
-sudo dnf install -y https://packages.groonga.org/almalinux/${os_version}/groonga-release-latest.noarch.rpm
+sudo dnf install -y "https://packages.groonga.org/almalinux/${os_version}/groonga-release-latest.noarch.rpm"
 sudo dnf module -y disable mariadb
 sudo dnf module -y disable mysql
 sudo dnf install -y --enablerepo=powertools mariadb-server
 sudo systemctl start mariadb
-sudo dnf install -y --enablerepo=powertools ${package}
+sudo dnf install -y --enablerepo=powertools "${package}"
 sudo mysql -e "SHOW ENGINES" | grep Mroonga
