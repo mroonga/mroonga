29c6c85196f5861f5856f83a5645939b29258082
Horimoto Yasuhiro <horimoto@clear-code.com>
2025-01-14 18:08:01 +0900

    ci: add install test for AlmaLinux 8 with MySQL Community Server 8.0

--- packages/yum/install_test.sh
+++ packages/yum/install_test.sh
@@ -5,19 +5,10 @@ set -exu
 package="$1"
 
 os_version=$(cut -d: -f5 /etc/system-release-cpe)
-mariadb_version=$(echo "${package}" | cut -d'-' -f2)
-
-cat <<REPO | sudo tee /etc/yum.repos.d/MariaDB.repo
-[mariadb]
-name = MariaDB
-baseurl = "https://rpm.mariadb.org/${mariadb_version}/rhel/\$releasever/\$basearch"
-gpgkey = https://rpm.mariadb.org/RPM-GPG-KEY-MariaDB
-gpgcheck = 1
-REPO
 
 case "${os_version}" in
   8)
-    DNF_INSTALL="dnf install -y --enablerepo=powertools"
+    DNF_INSTALL="dnf install -y --enablerepo=epel,powertools --disablerepo=AppStream"
     sudo dnf module -y disable mysql
     ;;
   *)
@@ -27,8 +18,29 @@ case "${os_version}" in
 esac
 
 sudo ${DNF_INSTALL} "https://packages.groonga.org/almalinux/${os_version}/groonga-release-latest.noarch.rpm"
-sudo dnf module -y disable mariadb
-sudo ${DNF_INSTALL} mariadb-server
-sudo systemctl start mariadb
+
+case "${package}" in
+  mariadb-*)
+    mariadb_version=$(echo "${package}" | cut -d'-' -f2)
+
+    cat <<REPO | sudo tee /etc/yum.repos.d/MariaDB.repo
+[mariadb]
+name = MariaDB
+baseurl = "https://rpm.mariadb.org/${mariadb_version}/rhel/\$releasever/\$basearch"
+gpgkey = https://rpm.mariadb.org/RPM-GPG-KEY-MariaDB
+gpgcheck = 1
+REPO
+    sudo dnf module -y disable mariadb
+    sudo ${DNF_INSTALL} mariadb-server
+    sudo systemctl start mariadb
+    ;;
+  mysql-community-*)
+    mysql_version=$(echo "${package}" | cut -d'-' -f3)
+    mysql_package_version=$(echo "${mysql_version}" | sed -e 's/\.//g')
+
+    sudo ${DNF_INSTALL} \
+         "https://repo.mysql.com/mysql${mysql_package_version}-community-release-el${os_version}.rpm"
+esac
+
 sudo ${DNF_INSTALL} "${package}"
 sudo mysql -e "SHOW ENGINES" | grep Mroonga
