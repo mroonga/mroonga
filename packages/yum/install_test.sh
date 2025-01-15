d946db71190b010869d0a5f67060eae974853ac6
Horimoto Yasuhiro <horimoto@clear-code.com>
2025-01-15 12:57:42 +0900

    ci: add install test for AlmaLinux 8 with MySQL Community Server 8.0 (#849)
    Related: GitHub: GH-837
    This adds support for only AlmaLinux 8 and MySQL Community Server 8.0.

--- packages/yum/install_test.sh
+++ packages/yum/install_test.sh
@@ -5,15 +5,6 @@ set -exu
 package="$1"
 
 os_version=$(cut -d: -f5 /etc/system-release-cpe)
-mariadb_version=$(echo "${package}" | cut -d'-' -f2)
-
-cat <<REPO | sudo tee /etc/yum.repos.d/MariaDB.repo
-[mariadb]
-name = MariaDB
-baseurl = "https://rpm.mariadb.org/${mariadb_version}/rhel/\$releasever/\$basearch"
-gpgkey = https://rpm.mariadb.org/RPM-GPG-KEY-MariaDB
-gpgcheck = 1
-REPO
 
 case "${os_version}" in
   8)
@@ -27,8 +18,39 @@ case "${os_version}" in
 esac
 
 sudo ${DNF_INSTALL} "https://packages.groonga.org/almalinux/${os_version}/groonga-release-latest.noarch.rpm"
-sudo dnf module -y disable mariadb
-sudo ${DNF_INSTALL} mariadb-server
-sudo systemctl start mariadb
+
+case "${package}" in
+  mariadb-*)
+    mariadb_version=$(echo "${package}" | cut -d'-' -f2)
+    service_name=mariadb
+    have_auto_generated_password="no"
+
+    cat <<REPO | sudo tee /etc/yum.repos.d/MariaDB.repo
+[mariadb]
+name = MariaDB
+baseurl = "https://rpm.mariadb.org/${mariadb_version}/rhel/\$releasever/\$basearch"
+gpgkey = https://rpm.mariadb.org/RPM-GPG-KEY-MariaDB
+gpgcheck = 1
+REPO
+    sudo dnf module -y disable mariadb
+    ;;
+  mysql-community-*)
+    mysql_version=$(echo "${package}" | cut -d'-' -f3)
+    mysql_package_version=$(echo "${mysql_version}" | sed -e 's/\.//g')
+    service_name=mysqld
+    have_auto_generated_password="yes"
+
+    sudo ${DNF_INSTALL} \
+         "https://repo.mysql.com/mysql${mysql_package_version}-community-release-el${os_version}.rpm"
+esac
+
 sudo ${DNF_INSTALL} "${package}"
-sudo mysql -e "SHOW ENGINES" | grep Mroonga
+sudo systemctl start "${service_name}"
+mysql="mysql -u root"
+if [ "${have_auto_generated_password}" = "yes" ]; then
+  auto_generated_password=$(sudo awk '/root@localhost/{print $NF}' /var/log/mysqld.log | tail -n 1)
+  mysql="mysql -u root -p${auto_generated_password}"
+  sudo ${mysql} --connect-expired-password -e "ALTER USER user() IDENTIFIED BY '$auto_generated_password'"
+fi
+
+sudo ${mysql} -e "SHOW ENGINES" | grep Mroonga
