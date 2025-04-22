0e34b86a3834abb9a21a52169535666350a49d64
otegami <a.s.takuya1026@gmail.com>
2025-04-22 17:07:10 +0800

    Add a comment explaining why we use a URI without a trailing slash

--- packages/mroonga-package-task.rb
+++ packages/mroonga-package-task.rb
@@ -237,7 +237,9 @@ class MroongaPackageTask < PackagesGroongaOrgPackageTask
 
   def detect_mysql_community_rpm_version
     series = split_mysql_package[1]
-    # CDN edge nodes may return old cached list without a cache‑buster.
+    # Use the URL without a trailing slash because the CDN caches both “…/SRPMS”
+    # and “…/SRPMS/”, but the cache for the “/SRPMS/” path hasn't been expired
+    # yet. Using the no‑slash URL ensures we get the latest listing.
     srpms_url =
       "https://repo.mysql.com/yum/mysql-#{series}-community/el/9/SRPMS"
     index_html = URI.open(srpms_url) do |response|
@@ -258,7 +260,9 @@ class MroongaPackageTask < PackagesGroongaOrgPackageTask
 
   def detect_mysql_community_minimal_rpm_version
     series = split_mysql_package[1]
-    # CDN edge nodes may return old cached list without a cache‑buster.
+    # Use the URL without a trailing slash because the CDN caches both “…/SRPMS”
+    # and “…/SRPMS/”, but the cache for the “/SRPMS/” path hasn't been expired
+    # yet. Using the no‑slash URL ensures we get the latest listing.
     srpms_url =
       "https://repo.mysql.com/yum/mysql-#{series}-community/docker/el/9/SRPMS"
     index_html = URI.open(srpms_url) do |response|
