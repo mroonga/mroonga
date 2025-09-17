f41e58a9c8b80eba0f680a821faebd5a59f6a97b
Horimoto Yasuhiro <horimoto@clear-code.com>
2025-09-17 21:17:16 +0900

    ci install: add a install test for MariaDB with Debian 12

--- packages/apt/install_test.sh
+++ packages/apt/install_test.sh
@@ -3,16 +3,31 @@
 set -exu
 
 package=$1
+distribution=$(lsb_release --id --short | tr 'A-Z' 'a-z')
+code_name=$(lsb_release --codename --short)
 
 sudo apt update
-sudo apt install -y -V software-properties-common lsb-release
-sudo add-apt-repository -y universe
-sudo add-apt-repository \
-     "deb http://security.ubuntu.com/ubuntu $(lsb_release --short --codename)-security main restricted"
 
-sudo add-apt-repository -y ppa:groonga/ppa
+case ${distribution} in
+  debian)
+    sudo apt install -y -V wget
+    wget https://apache.jfrog.io/artifactory/arrow/debian/apache-arrow-apt-source-latest-${code_name}.deb
+    sudo apt install -y -V ./apache-arrow-apt-source-latest-${code_name}.deb
+    wget https://packages.groonga.org/debian/groonga-apt-source-latest-${code_name}.deb
+    sudo apt install -y -V ./groonga-apt-source-latest-${code_name}.deb
+  ;;
+  ubuntu)
+    sudo apt install -y -V software-properties-common lsb-release
+    sudo add-apt-repository -y universe
+    sudo add-apt-repository \
+         "deb http://security.ubuntu.com/ubuntu $(lsb_release --short --codename)-security main restricted"
+    sudo add-apt-repository -y ppa:groonga/ppa
+  ;;
+esac
+
 sudo apt update
 
 sudo apt install -V -y ${package}
+sudo apt install -y -V groonga-tokenizer-mecab
 
 sudo mysql -e "SHOW ENGINES" | grep Mroonga
