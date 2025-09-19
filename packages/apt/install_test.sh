d32064452a6c49003b2c8c277c8aec9c33136c4a
Horimoto Yasuhiro <horimoto@clear-code.com>
2025-09-19 11:54:36 +0900

    Use more suitable URL
    Co-authored-by: Abe Tomoaki <abe@enzou.tokyo>

--- packages/apt/install_test.sh
+++ packages/apt/install_test.sh
@@ -11,7 +11,7 @@ distribution=$(lsb_release --id --short | tr 'A-Z' 'a-z')
 code_name=$(lsb_release --codename --short)
 
 sudo apt install -y -V wget
-wget https://apache.jfrog.io/artifactory/arrow/${distribution}/apache-arrow-apt-source-latest-${code_name}.deb
+wget https://packages.apache.org/artifactory/arrow/${distribution}/apache-arrow-apt-source-latest-${code_name}.deb
 sudo apt install -y -V ./apache-arrow-apt-source-latest-${code_name}.deb
 wget https://packages.groonga.org/${distribution}/groonga-apt-source-latest-${code_name}.deb
 sudo apt install -y -V ./groonga-apt-source-latest-${code_name}.deb
