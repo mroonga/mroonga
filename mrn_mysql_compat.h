b8d5f61b5e1ebf4772fca4bbfaf47687570abe8a
Horimoto Yasuhiro <horimoto@clear-code.com>
2026-04-17 09:44:59 +0900

    Fix a typo

--- mrn_mysql_compat.h
+++ mrn_mysql_compat.h
@@ -929,7 +929,7 @@ using TABLE_LIST = Table_ref;
     }
 #endif
 
-#if defined(MRN_MARIADB_P) && (MYSQL_VERSION_ID >= 110800)
+#if defined(MRN_MARIADB_P) && (MYSQL_VERSION_ID >= 130000)
 #  define MRN_FIELD_ENUM_GET_TYPELIB(field_enum) (field_enum)->typelib_attr()
 #elif defined(MRN_MARIADB_P) &&                                                \
   ((MYSQL_VERSION_ID >= 110800) || (MYSQL_VERSION_ID < 110900))
