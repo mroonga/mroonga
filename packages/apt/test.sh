3102add29882f5020a86b90c077f72d09a6cb663
Horimoto Yasuhiro <horimoto@clear-code.com>
2026-08-06 17:17:47 +0900

    packages ubuntu-jammy: add MySQL 9.7 LTS (#1134)

--- packages/apt/test.sh
+++ packages/apt/test.sh
@@ -162,7 +162,7 @@ sudo mkdir -p plugin
 sudo cp -a /host/mysql-test/mroonga/ plugin/
 
 case ${package} in
-  mysql-8.*|mysql-community-8.*)
+  mysql-*)
     ( \
       echo "/usr/lib/mysql-test/var/ r,"; \
       echo "/usr/lib/mysql-test/var/** rwk,"; \
