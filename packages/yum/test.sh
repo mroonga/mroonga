be7a1cc01e8b1b07c2ab445a7ec059417706fa51
Horimoto Yasuhiro <horimoto@clear-code.com>
2026-08-18 14:56:53 +0900

    remove duplicate configuration

--- packages/yum/test.sh
+++ packages/yum/test.sh
@@ -61,10 +61,6 @@ REPO
     mysql_package_version=$(echo ${mysql_version} | sed -e 's/\.//g')
     mysql_command=mysql
 
-    sudo ${DNF} install -y \
-         https://repo.mysql.com/mysql-community-minimal-release-el${major_version}.rpm
-    echo "module_hotfixes=true" | sudo tee -a /etc/yum.repos.d/mysql-community-minimal.repo
-    sudo sed -i -e 's/^enabled=0/enabled=1/g' /etc/yum.repos.d/mysql-community-minimal.repo
     # Currently, mysql-community-minimal-release-el9.rpm does not contain SRPM repository configuration for MySQL 9.7 community minimal.
     # Therefore, we specify manually this configuration.
     if [ "${mysql_version}" = "9.7" ]; then
@@ -76,6 +72,11 @@ gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-mysql-2023
 enabled=1
 gpgcheck = 1
 REPO
+    else
+      sudo ${DNF} install -y \
+           https://repo.mysql.com/mysql-community-minimal-release-el${major_version}.rpm
+      echo "module_hotfixes=true" | sudo tee -a /etc/yum.repos.d/mysql-community-minimal.repo
+      sudo sed -i -e 's/^enabled=0/enabled=1/g' /etc/yum.repos.d/mysql-community-minimal.repo
     fi
     ;;
   mysql-community-*)
