a87f190834385a2d832467cd2ec8b3c139973d50
Horimoto Yasuhiro <horimoto@clear-code.com>
2026-08-21 12:29:52 +0900

    Debug

--- packages/yum/test.sh
+++ packages/yum/test.sh
@@ -169,6 +169,7 @@ function mroonga_can_be_registered_for_mysql_community_minimal() {
   sudo ${mysql} --connect-expired-password -e "ALTER USER user() IDENTIFIED BY '$auto_generated_password'"
 
   cat /usr/share/mroonga/install.sql
+  mysql --print-defaults
   sudo ${mysql} < /usr/share/mroonga/install.sql
   sudo ${mysql} -e "SHOW ENGINES" | grep Mroonga
   mysqladmin -u root -p${auto_generated_password} shutdown
