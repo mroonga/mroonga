39cf42c694a62e1f1a7ea3dd5de70679e47a459f
Horimoto Yasuhiro <horimoto@clear-code.com>
2026-08-24 11:40:22 +0900

    execute install.sql manually

--- packages/yum/test.sh
+++ packages/yum/test.sh
@@ -168,8 +168,8 @@ function mroonga_can_be_registered_for_mysql_community_minimal() {
 
   sudo ${mysql} --connect-expired-password -e "ALTER USER user() IDENTIFIED BY '$auto_generated_password'"
 
-  cat /usr/share/mroonga/install.sql
-  sudo ${mysql} < /usr/share/mroonga/install.sql
+  sudo ${mysql} -e "INSTALL PLUGIN mroonga SONAME 'ha_mroonga.so';"
+  sudo ${mysql} < /usr/share/mroonga/update.sql
   sudo ${mysql} -e "SHOW ENGINES" | grep Mroonga
   mysqladmin -u root -p${auto_generated_password} shutdown
 }
